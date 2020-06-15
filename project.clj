db1c198735f769f98cd3c8633191dd73379c5561
Baptiste Fontaine <baptiste.fontaine@oscaro.com>
2020-06-15 14:15:04 +0200

    Add CI

--- project.clj
+++ project.clj
@@ -3,6 +3,14 @@
   :url "https://github.com/oscaro/edn2json"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["snapshots" {:url "https://repo.clojars.org"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://repo.clojars.org"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [cheshire            "5.8.0"]]
   :main edn2json.core
